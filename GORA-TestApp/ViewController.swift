94b49e503c25d4457f49f0b7a7e534326b8822d0
panishev95 <andreypanishev@hotmail.com>
2021-06-23 20:11:42 +0300

    Initial Commit

---
+++ GORA-TestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GORA-TestApp
+//
+//  Created by Andrei Panishev on 23.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
